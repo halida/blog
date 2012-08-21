da9978bc92b93884bf8b97922f663d48d99b8fbb
halida <linjunhalida@gmail.com>
2012-08-21 21:54:38 +0800

    prepare move to octpress

---
+++ move.rb
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+BLOG_DIR = "."
+TARGET_DIR = "/data/workspace/haliblog-octopress/source/_posts"
+
+FORMAT = """
+---
+layout: post
+title: ""%{title}""
+date: %{created}
+comments: true
+categories: 
+---
+"""
+
+maped_title = File.open("#{BLOG_DIR}/name_map.txt").read().split("\n").map{|line| line.split}
+maped_title = Hash[maped_title]
+MAPED_TITLE = maped_title
+
+# 把文档rst变成markdown， 交给octopress用
+def move
+  # 更新
+  `git pull`
+
+  Dir.chdir BLOG_DIR
+
+  # 获取列表
+  lists = Dir.glob('*.rst')
+  # # 不获取临时文件(_开头)
+  # lists.reject!{|file| File.basename(file)[0] == '_'}
+
+  # 生成时间, 文件名列表
+  infos = lists.map do |file|
+    [
+     # created
+     %x{git log --format=%ai "#{file}"| tail -1},
+     # updated
+     %x{git log --format=%ai "#{file}"| head -1},
+     File.basename(file, '.rst')
+    ]
+  end
+
+  # 按时间排序
+  infos.sort!
+
+  #生成新的文件
+  infos.each do |created, updated, title|
+    cmd = %{cat "#{title}.rst"| pandoc  -f rst -t markdown}
+    text = `#{cmd}`
+    # code
+    text = text.strip.gsub(/~~~~/, "```").gsub(/~~~~ {.sourceCode .(\w+)}/, '```\1')
+    # first line image need a space
+    text.sub!(/\n/, "\n\n") if text.start_with? "![image]"
+
+    header = FORMAT % {title: title, created: created}
+    File.open("#{TARGET_DIR}/#{created.split[0]}-#{MAPED_TITLE[title]}.markdown", "w+").write("#{header}\n#{text}")
+  end  
+end
+
+move()
+
